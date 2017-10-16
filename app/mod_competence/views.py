8d67d88a3ab9ddfb7cf2a4b44b63e1dfa61dbc46
CooperNh <nicholacooper1@gmail.com>
2017-10-16 09:13:23 +0100

    Add Competence update

--- app/mod_competence/views.py
+++ app/mod_competence/views.py
@@ -1,16 +1,38 @@
 from collections import OrderedDict
 
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from flask_table import Table, Col
+from sqlalchemy import and_, or_, case
 from flask import render_template, request, url_for, redirect, Blueprint
 from flask.ext.login import login_required, current_user
 from app.views import admin_permission
 from app.models import *
 from app.competence import s
 from forms import *
+import json
 
 competence = Blueprint('competence', __name__, template_folder='templates')
+
+class ItemTableSubsections(Table):
+    name = Col('Area of Competence')
+    evidence = Col('Evidence Type')
+    comments = Col('Comments')
 
 @competence.route('/add', methods=['GET', 'POST'])
 def add_competence():
     form = AddCompetence()
     return render_template('competence_add.html', form=form)
+
+@competence.route('/section', methods=['GET', 'POST'])
+def get_section():
+    if request.method == 'POST':
+        # add subsection section database
+        pass
+    text = request.args.get('text')
+    val = request.json['val']
+    c_id = request.json['c_id']
+
+    form = SectionForm()
+    result = s.query(Subsection).join(Competence).join(Section).filter(and_(Competence.id==c_id, Section.id==val)).values(Subsection.name, Subsection.evidence, Subsection.comments)
+    table = ItemTableSubsections(result, classes=['table', 'table-striped'])
+    return jsonify(render_template('section.html', form=form, val=val, text=text, table=table))
