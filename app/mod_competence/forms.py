37ca8861bc9bbf008c0457166c0ef7f4fcda99c9
CooperNh <nicholacooper1@gmail.com>
2017-10-17 13:30:25 +0100

    Added new methods

--- app/mod_competence/forms.py
+++ app/mod_competence/forms.py
@@ -20,20 +20,9 @@ class AddCompetence(Form):
     scope = TextField("Scope", [Required("Enter a Scope")])
     #qpulsenum = TextField("QPulse Doc ID", [Required("Enter a QPulse ID")]) #This needs to be added after competency creation!
     creator_id = TextField("Author Name", [Required("Enter an Author Name")])
-    val_periods = ['6 Months', '1 Year', '2 Years']
     validity_period = QuerySelectField("Validity Period", query_factory=lambda:s.query(ValidityRef).all(), get_label="months")
     documents = QuerySelectMultipleField("Associated Documents",query_factory=lambda:s.query(Documents).all(), get_label="qpulse_no")
     add_document = TextField("Add Document", [Required("Enter a Q-Pulse Document Number")])
-
-    # h_and_s = QuerySelectMultipleField("Relevant Health and Safety", query_factory=lambda:s.query(HealthSafetyRef).all(), get_label="question") #This needs to be then added as a (constant) subsection to the competence
-    # add_h_and_s = TextField("Add Health and Safety Hazard")
-    # coshh = QuerySelectMultipleField("Related COSHH", query_factory=lambda: s.query(CoshhRef).all(),get_label="coshhitem") #This needs to be then added as a (constant) subsection to the competence
-    # add_coshh = TextField("Add COSHH Item")
-    # reagent_handling = QuerySelectMultipleField("Relevant reagent handling and storage", query_factory=lambda: s.query(ReagentRef).all(), get_label="reagent") #This needs to be then added as a (constant) subsection to the competence
-    # add_reagent = TextField("Add Reagent Handling or Storage Item")
-    #
-    # choose_section = QuerySelectField("Add Section" ,query_factory=lambda:s.query(Section).all(), get_label="name") #All sections in database
-    # competence_sections = QuerySelectField("Add Section" ,query_factory=lambda:s.query(Section).all(), get_label="name") #All sections that are added for that specific competency
 
     submit = SubmitField()
 
--- app/mod_competence/views.py
+++ app/mod_competence/views.py
@@ -21,17 +21,49 @@ class ItemTableSubsections(Table):
 @competence.route('/add', methods=['GET', 'POST'])
 def add_competence():
     form = AddCompetence()
+    if request.method == 'POST':
+        print current_user.database_id
+        title = request.form['title']
+        print title
+        scope = request.form['scope']
+        print scope
+        val_period = request.form['validity_period']
+        print val_period
+        c = Competence(title=title, creator_id=current_user.database_id, scope=scope, validity_period=val_period)
+        s.add(c)
+        s.commit()
+        c_id = c.id
+        print c_id
+
     return render_template('competence_add.html', form=form)
 
 @competence.route('/addsections', methods=['GET', 'POST'])
 def add_sections():
     form = AddSection()
-
     return render_template('competence_section.html', form=form)
+
+@competence.route('/section', methods=['GET', 'POST'])
+def get_section():
+
+    if request.method == 'POST':
+        # add subsection section database
+        pass
+    text = request.json['text']
+    val = request.json['val']
+    c_id = request.json['c_id']
+
+    form = SectionForm()
+    subsection_form = AddSubsection()
+    #method below gets the subsections for the section_id selected in the form
+    result = s.query(Subsection).join(Competence).join(Section).join(EvidenceTypeRef).filter(and_(Competence.id==c_id, Section.id==val)).values(Subsection.name, EvidenceTypeRef.type, Subsection.comments)
+    table = ItemTableSubsections(result, classes=['table', 'table-striped', 'section_'+str(val)])
+    print str(c_id) + ' ' + str(val) + ' ' + 'should get subsections for selected section'
+    return jsonify(render_template('section.html',c_id=c_id, form=form, val=val, text=text, table=table, subsection_form=subsection_form))
+
 
 @competence.route('/add_subsection_to_db', methods=['GET', 'POST'])
 def add_sections_to_db():
-
+    #method adds subsections to database
     name = request.json['name']
     evidence_id = request.json['evidence_id']
     comments = request.json['comments']
@@ -43,27 +75,10 @@ def add_sections_to_db():
     result = s.query(Subsection).join(Competence).join(Section).join(EvidenceTypeRef).filter(Competence.id == c_id).filter(Section.id == s_id). \
         values(Subsection.name, EvidenceTypeRef.type, Subsection.comments)
 
-    table = ItemTableSubsections(result, classes=['table', 'table-striped', 'section'])
+    table = ItemTableSubsections(result, classes=['table', 'table-striped', 'section_'+str(s_id)])
+    print str(c_id) + ' ' + str(s_id) + ' ' + 'should add new subsection to selected section'
     return jsonify(table)
 
-
-
-@competence.route('/section', methods=['GET', 'POST'])
-def get_section():
-    """
-
-
-    :return:
-    """
-    if request.method == 'POST':
-        # add subsection section database
-        pass
-    text = request.json['text']
-    val = request.json['val']
-    c_id = request.json['c_id']
-
-    form = SectionForm()
-    subsection_form = AddSubsection()
-    result = s.query(Subsection).join(Competence).join(Section).join(EvidenceTypeRef).filter(and_(Competence.id==c_id, Section.id==val)).values(Subsection.name, EvidenceTypeRef.type, Subsection.comments)
-    table = ItemTableSubsections(result, classes=['table', 'table-striped', 'section'])
-    return jsonify(render_template('section.html',c_id=c_id, form=form, val=val, text=text, table=table, subsection_form=subsection_form))
+def get_constants():
+    #Method to get all subsections that have a constant flag in the database
+    pass
