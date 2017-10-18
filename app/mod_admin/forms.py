9f1fb98ec8877d30daee3ebb4438e7d7bd09e893
mattdparker <matthewdanielparker@gmail.com>
2017-10-18 13:18:13 +0100

    Merge branch 'develop_lizzy' of https://github.com/sch-sdgs/SDGSCompetence
    # Conflicts:
    #	.gitignore
    #	.idea/workspace.xml

--- app/mod_admin/forms.py
+++ app/mod_admin/forms.py
@@ -16,9 +16,12 @@ class UserForm(Form):
     firstname = TextField("First Name", [Required("Enter a Username")])
     surname = TextField("Surname", [Required("Enter a Username")])
     email = TextField("Email", [Required("Enter a Username")])
+    staff_no = TextField("Staff Number", [Required("Enter a Username")])
     linemanager = TextField("Line Manager")
     jobrole = QuerySelectMultipleField("Job Role", query_factory=lambda: s.query(JobRoles).all(), get_label="job")
     userrole = QuerySelectMultipleField("User Role", query_factory=lambda: s.query(UserRolesRef).all(), get_label="role")
+    section = QuerySelectField("Section", query_factory=lambda: s.query(Service).all(),
+                                        get_label="name")
     submit = SubmitField()
 
 
@@ -27,9 +30,11 @@ class UserEditForm(Form):
     firstname = TextField("First Name", [Required("Enter a Username")])
     surname = TextField("Surname", [Required("Enter a Username")])
     email = TextField("Email", [Required("Enter a Username")])
+    staff_no = TextField("Staff Number", [Required("Enter a Username")])
     linemanager = TextField("Line Manager")
     jobrole = SelectMultipleField("Job Role")
     userrole = SelectMultipleField("User Role")
+    section = QuerySelectField("Section")
     submit = SubmitField()
 
 class EvidenceTypeForm(Form):
