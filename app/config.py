a07df17b26a090847613353ddde5991302ec1bfc
mattparker <matthewdanielparker@gmail.com>
2018-07-16 11:06:32 +0100

    Merge branch 'develop_christine' of https://github.com/sch-sdgs/SDGSCompetence
    # Conflicts:
    #	app/run.py

--- app/config.py
+++ app/config.py
@@ -6,21 +6,19 @@ basedir = os.path.dirname(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'mysql://stardb:stardb@0.0.0.0:81/stardb_dev'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 WHOOSH_BASE = os.path.join(basedir + '/app/resources/')
-#UPLOAD_FOLDER = os.path.join(basedir + '/app/static/uploads')
 UPLOAD_FOLDER = os.path.join('/uploads')
-#UPLOADED_FILES_DEST = os.path.join(basedir + '/app/static/uploads')
 UPLOADED_FILES_DEST = os.path.join('/uploads')
 QPULSE_MODULE=False
 
-#
-# MAIL_SERVER = 'smtp.sch.nhs.uk'
-# MAIL_PORT = 25
-# MAIL_USERNAME = None
-# MAIL_PASSWORD = None
 
-MAIL_SERVER = 'smtp.mailtrap.io'
-MAIL_PORT = '2525'
-MAIL_USERNAME = "80753817cf5a63"
-MAIL_PASSWORD = "c69cbe5572dd88"
+MAIL_SERVER = 'smtp.sch.nhs.uk'
+MAIL_PORT = 25
+MAIL_USERNAME = None
+MAIL_PASSWORD = None
+#
+# MAIL_SERVER = 'smtp.mailtrap.io'
+# MAIL_PORT = '2525'
+# MAIL_USERNAME = "80753817cf5a63"
+# MAIL_PASSWORD = "c69cbe5572dd88"
 
 TRELLO_APP_KEY = '552b7506965909b0d129018876f692d3'
